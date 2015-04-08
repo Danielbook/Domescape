83cac1d1dfd771fb304e9a134f19ca6e9595dcd7
Daniel Böök <Daniel.book@hotmail.com>
2015-04-08 11:44:31 +0200

    added some basic files

---
+++ Cmake/CMakeFiles/example1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example1.dir/main.cpp.o"
+  "/Users/Daniel/Downloads/sgct_2.5.2_cpp11/examples/example1/example1.pdb"
+  "/Users/Daniel/Downloads/sgct_2.5.2_cpp11/examples/example1/example1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
