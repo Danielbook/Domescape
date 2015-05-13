316582598de1324425ffd64f83c6f243bf6840c3
Adam Alsegård <adam.alsegard@gmail.com>
2015-05-13 10:01:22 +0200

    Removed heightmap. Correct landscape/MVstack. Incomplete (Commented) sky shader and shadowmap.

--- project/main.cpp
+++ project/main.cpp
@@ -66,12 +66,12 @@ float calcSunPosition(); // Calculates the suns position
 void resetToCurrentTime(); // Used to calculate the time of the current computer
 /*-----------------------------------------------------*/
 
-/*------------------HEIGHTMAP------------------*/
-void generateTerrainGrid( float width, float height, unsigned int wRes, unsigned int dRes );
-void initHeightMap();
-void drawHeightMap(glm::mat4 MVP, glm::mat3 NM, glm::mat4 MV, glm::mat4 MV_light, glm::vec3 lDir, float fAmb);
-
+/*------------------HEIGHTMAP->SHADOWMAP?------------------*/
+//Implementera i Shadowmap!?
 //opengl objects
+GLuint FramebufferName = 0;
+GLuint depthTexture;
+
 GLuint vertexArray = GL_FALSE;
 GLuint vertexPositionBuffer = GL_FALSE;
 GLuint texCoordBuffer = GL_FALSE;
@@ -91,22 +91,10 @@ GLsizei mNumberOfVerts = 0;
 /*---------------------------------------------*/
 
 /*------------------SHADERS------------------*/
-//Heightmap shader
-sgct::ShaderProgram mSp;
-GLint myTextureLocations[]	= { -1, -1 };
-GLint MVP_Loc_G = -1;
-GLint MV_Loc_G = -1;
-GLint MVL_Loc_G = -1;
-GLint NM_Loc_G = -1;
-GLint lDir_Loc_G = -1;
-GLint Amb_Loc_G = -1;
-
 //Shader Shadowmap
 GLint depthMVP_Loc = -1;
 GLint depthBiasMVP_Loc = -1;
 GLint shadowmap_Loc = -1;
-GLuint FramebufferName = 0;
-GLuint depthTexture;
 
 //Shader Scene
 GLint MVP_Loc = -1;
@@ -142,7 +130,7 @@ glm::vec3 bView(0.0f, 0.0f, 0.0f);
 glm::vec3 up(0.0f, 1.0f, 0.0f);
 glm::vec3 pos(0.0f, 0.0f, 0.0f);
 
-
+//Fullösning
 float sunX = 500.0f;
 float sunY = 100.f;
 glm::vec3 sunPosition(sunX, sunY, 0.0f);
@@ -255,8 +243,6 @@ void myInitOGLFun(){
     sgct::TextureManager::instance()->setAnisotropicFilterSize(4.0f);
     sgct::TextureManager::instance()->setCompression(sgct::TextureManager::S3TC_DXT);
 
-    // TO BE REPLACED
-    //initHeightMap();
 
     /*----------------OBJECTS AND TEXTURES--------------*/
 
@@ -317,7 +303,7 @@ void myInitOGLFun(){
     glUniform1i( Tex_Loc, 0 );
     depthBiasMVP_Loc = sgct::ShaderManager::instance()->getShaderProgram( "scene").getUniformLocation( "depthBiasMVP" );
     shadowmap_Loc = sgct::ShaderManager::instance()->getShaderProgram( "scene").getUniformLocation( "shadowMap" );
-    glUniform1i( shadowmap_Loc, 0 );
+    glUniform1i( shadowmap_Loc, 1 );
 
     sgct::ShaderManager::instance()->unBindShaderProgram();
 
@@ -540,10 +526,7 @@ void myDrawFun(){
     //create scene transform (animation)
     glm::mat4 scene_mat = xform.getVal();
 
-    //För heightmap - onödiga?
     glm::mat4 MV = gEngine->getActiveModelViewMatrix() * scene_mat;
-    glm::mat4 MV_light = gEngine->getActiveModelViewMatrix();
-    glm::mat3 NML = glm::inverseTranspose(glm::mat3( MV_light ));
 
     gEngine->setNearAndFarClippingPlanes(0.1f, 2000.0f);
     glm::mat4 MVP = gEngine->getActiveModelViewProjectionMatrix() * scene_mat;
@@ -582,9 +565,6 @@ void myDrawFun(){
     }
 
     glm::vec3 lDir = glm::normalize(vSunPos);
-
-    //TO BE REPLACED
-    //drawHeightMap(MVP, NML, MV, MV_light, lDir, fAmb);
 
 
 
@@ -869,159 +849,6 @@ void externalControlStatusCallback( bool connected ){
         sgct::MessageHandler::instance()->print("External control disconnected.\n");
 }
 
-void initHeightMap(){
-    //setup textures
-    sgct::TextureManager::instance()->loadTexure("heightmap", "texture/map.png", true, 0);
-    sgct::TextureManager::instance()->loadTexure("normalmap", "texture/normal.png", true, 0);
-
-    //setup shader
-    sgct::ShaderManager::instance()->addShaderProgram( mSp, "Heightmap", "shaders/heightmap.vert", "shaders/heightmap.frag" );
-
-    mSp.bind();
-    myTextureLocations[0]	= mSp.getUniformLocation( "hTex" );
-    myTextureLocations[1]	= mSp.getUniformLocation( "nTex" );
-
-    MVP_Loc_G		= mSp.getUniformLocation( "MVP" );
-    MV_Loc_G		= mSp.getUniformLocation( "MV" );
-    MVL_Loc_G		= mSp.getUniformLocation( "MV_light" );
-    NM_Loc_G		= mSp.getUniformLocation( "normalMatrix" );
-    lDir_Loc_G		= mSp.getUniformLocation( "light_dir" );
-    Amb_Loc_G       = mSp.getUniformLocation( "lAmb");
-    glUniform1i( myTextureLocations[0], 0 );
-    glUniform1i( myTextureLocations[1], 1 );
-
-    sgct::ShaderManager::instance()->unBindShaderProgram();
-
-    //generate mesh
-    generateTerrainGrid( 250.0f, 250.0f, 1024, 1024  );
-
-    //generate vertex array
-    glGenVertexArrays(1, &vertexArray);
-    glBindVertexArray(vertexArray);
-
-    //generate vertex position buffer
-    glGenBuffers(1, &vertexPositionBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexPositionBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * mVertPos.size(), &mVertPos[0], GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(
-                          0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-                          3,                  // size
-                          GL_FLOAT,           // type
-                          GL_FALSE,           // normalized?
-                          0,                  // stride
-                          reinterpret_cast<void*>(0) // array buffer offset
-                          );
-
-    //generate texture coord buffer
-    glGenBuffers(1, &texCoordBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, texCoordBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * mTexCoord.size(), &mTexCoord[0], GL_STATIC_DRAW);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(
-                          1,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-                          2,                  // size
-                          GL_FLOAT,           // type
-                          GL_FALSE,           // normalized?
-                          0,                  // stride
-                          reinterpret_cast<void*>(0) // array buffer offset
-                          );
-
-    glBindVertexArray(0); //unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    //cleanup
-    mVertPos.clear();
-    mTexCoord.clear();
-}
-
-
-void drawHeightMap(glm::mat4 MVP, glm::mat3 NM, glm::mat4 MV, glm::mat4 MV_light, glm::vec3 lDir, float fAmb)
-{
-    glEnable(GL_CULL_FACE);
-
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, sgct::TextureManager::instance()->getTextureId( "heightmap" ));
-
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, sgct::TextureManager::instance()->getTextureId( "normalmap" ));
-
-    mSp.bind();
-
-    glUniformMatrix4fv(MVP_Loc_G, 1, GL_FALSE, &MVP[0][0]);
-    glUniformMatrix4fv(MV_Loc_G, 1, GL_FALSE, &MV[0][0]);
-    glUniformMatrix4fv(MVL_Loc_G, 1, GL_FALSE, &MV_light[0][0]);
-    glUniformMatrix3fv(NM_Loc_G, 1, GL_FALSE, &NM[0][0]);
-    glUniform3fv(lDir_Loc_G, 1, &lDir[0]);
-    glUniform1fv(Amb_Loc_G, 1, &fAmb);
-
-    glBindVertexArray(vertexArray);
-
-    // Draw the triangle !
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, mNumberOfVerts);
-
-    //unbind
-    glBindVertexArray(0);
-
-    mSp.unbind();
-}
-
-/*!
- Will draw a flat surface that can be used for the heightmapped terrain.
- @param	width	Width of the surface
- @param	depth	Depth of the surface
- @param	wRes	Width resolution of the surface
- @param	dRes	Depth resolution of the surface
- */
-void generateTerrainGrid( float width, float depth, unsigned int wRes, unsigned int dRes )
-{
-    float wStart = -width * 0.5f;
-    float dStart = -depth * 0.5f;
-
-
-    float dW = width / static_cast<float>( wRes );
-    float dD = depth / static_cast<float>( dRes );
-
-    //cleanup
-    mVertPos.clear();
-    mTexCoord.clear();
-
-
-    for( unsigned int depthIndex = 0; depthIndex < dRes; ++depthIndex )
-    {
-        float dPosLow = dStart + dD * static_cast<float>( depthIndex );
-        float dPosHigh = dStart + dD * static_cast<float>( depthIndex + 1 );
-        float dTexCoordLow = depthIndex / static_cast<float>( dRes );
-        float dTexCoordHigh = (depthIndex+1) / static_cast<float>( dRes );
-
-        for( unsigned widthIndex = 0; widthIndex < wRes; ++widthIndex )
-        {
-            float wPos = wStart + dW * static_cast<float>( widthIndex );
-            float wTexCoord = widthIndex / static_cast<float>( wRes );
-
-            //p0
-            mVertPos.push_back( wPos ); //x
-            mVertPos.push_back( 0.0f ); //y
-            mVertPos.push_back( dPosLow ); //z
-
-            //p1
-            mVertPos.push_back( wPos ); //x
-            mVertPos.push_back( 0.0f ); //y
-            mVertPos.push_back( dPosHigh ); //z
-
-
-            //tex0
-            mTexCoord.push_back( wTexCoord ); //s
-            mTexCoord.push_back( dTexCoordLow ); //t
-
-            //tex1
-            mTexCoord.push_back( wTexCoord ); //s
-            mTexCoord.push_back( dTexCoordHigh ); //t
-        }
-    }
-
-    mNumberOfVerts = static_cast<GLsizei>(mVertPos.size() / 3); //each vertex has three componets (x, y & z)
-}
 
 /*
  http://en.cppreference.com/w/cpp/chrono/c/strftime
